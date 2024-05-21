07ef7fc4f0320fee635b6e4cc5436131c5227f6d
yelias-ambalait <yelias.ambala@gmail.com>
2024-05-21 09:57:34 +0600

    updated

--- s.js
+++ s.js
@@ -8,17 +8,26 @@
 
 // });
 
-const prompt = require("prompt-sync")({ sigint: true }); //npm install prompt-sync
-let grade = prompt("enter grade: ");
+// const prompt = require("prompt-sync")({ sigint: true }); //npm install prompt-sync
+// let grade = prompt("enter grade: ");
 
-if (grade >= 90) {
-  console.log("Excellent!");
-} else if (grade >= 80) {
-  console.log("Very Good!");
-} else {
-  console.log("Good job, keep practicing!");
-}
+// if (grade >= 90) {
+//   console.log("Excellent!");
+// } else if (grade >= 80) {
+//   console.log("Very Good!");
+// } else {
+//   console.log("Good job, keep practicing!");
+// }
 
 // for (let i = 0; i < 5; i++) {
 //   console.log("Iteration", i + 1);
 // }
+
+
+const prompt = require("prompt-sync")({ sigint: true });
+let rows = prompt("Enter number of rows: ");
+
+for(var i=1;i<=rows;i++){
+  
+
+}
