d65d7a60788e792e1f11112a7293e00a180d999b
yelias-ambalait <yelias.ambala@gmail.com>
2024-05-21 09:57:12 +0600

    first commit

---
+++ s.js
@@ -0,0 +1,24 @@
+// let form = document.getElementById("myForm");
+
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault(); // Prevent default form submission
+
+//   let name = document.getElementById("name").value;
+//   console.log("Hello", name + "!");
+
+// });
+
+const prompt = require("prompt-sync")({ sigint: true }); //npm install prompt-sync
+let grade = prompt("enter grade: ");
+
+if (grade >= 90) {
+  console.log("Excellent!");
+} else if (grade >= 80) {
+  console.log("Very Good!");
+} else {
+  console.log("Good job, keep practicing!");
+}
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("Iteration", i + 1);
+// }
